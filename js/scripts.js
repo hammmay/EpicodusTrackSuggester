978f60805ae94583944ac98363cc06a8abb9e098
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-02 11:00:12 -0700

    add radio buttons and submit button

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $("button#submit").click(function() {
+      $("ul").prepend("<li>"You hit the submit button"</li>");
+
+
+
+)};
+
+
+
+var question1 = $("input:radio[name=question1]:checked").val();
