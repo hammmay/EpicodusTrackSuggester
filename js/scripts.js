6687f78e3baebf89d773d092a772dc473f6cf2d5
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-02 15:37:57 -0700

    fix conditional if and add question copy

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
   $("form#studentInput").submit(function(event) {
     var question1 = $("select#question1").val();
     var question2 = $("select#question2").val();
+    var question3 = $("select#question3").val();
+    var question4 = $("select#question4").val();
+    var question5 = $("select#question5").val();
 
-
-    var quote = ("Both answers are Yes");
-    if (question1 === "yes" && question2 === "yes") {
-      quote === ("Yes and YES");
+    var tracks = ("Both answers are yes");
+    if (question1 === "no" && question2 === "yes") {
+      tracks = ("That's too bad. You have to at least want to code to be a good fit for any tracks Epicodus has to offers.");
     }
 
-    $("#rate").empty().append(quote);
-    $("#quote").show();
+    $("#options").empty().append(tracks);
+    $("#tracks").show();
 
     event.preventDefault();
   });
 });
-
-// NOTE: question1 = age
-// NOTE: question2 = gender
