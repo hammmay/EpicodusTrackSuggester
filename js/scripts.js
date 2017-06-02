75d642c45560bec508b59df5226badbb554f5dc7
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-02 16:25:01 -0700

    add additional if conditions and add answer copy

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,21 @@ $(document).ready(function() {
     var question4 = $("select#question4").val();
     var question5 = $("select#question5").val();
 
-    var tracks = ("Both answers are yes");
-    if (question1 === "no" && question2 === "yes") {
+    var tracks = ("Error");
+    if (question1 === "no") {
       tracks = ("That's too bad. You have to at least want to code to be a good fit for any tracks Epicodus has to offers.");
+    }
+    if (question2 === "yes" && question1 === "yes") {
+      tracks = ("C# and .NET is the best way to go. C# is the main language they use at Microsoft.");
+    }
+    if (question3 === "yes" && question1 === "yes") {
+      tracks = ("You should consider the CSS and Design course where you'll learn how to make everything pretty from the back end.");
+    }
+    if (question4 === "yes" && question1 === "yes") {
+      tracks = ("The Java and Android track will give you a solid foundation in programing and you'll even get to build cool Android apps.");
+    }
+    if (question5 === "yes" && question1 === "yes") {
+      tracks = ("It's normal to be nervous, especially if you have no experience in programing. But don't worry! The instructors at Epicodus are here to answer your questions about the different options and why they might be a good fit.");
     }
 
     $("#options").empty().append(tracks);
